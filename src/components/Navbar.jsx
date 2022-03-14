9bc0663da672b93749d39c3578b00de1f46ff152
Natalie Acevedo <natalieacevedo25@gmail.com>
2022-03-14 11:31:48 +0000

    adding everything so far

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,3 +9,49 @@ function Navbar() {
 }
 
 export default Navbar;
+
+// eslint-disable-next-line prettier/prettier
+
+// function App() {
+
+//     const dataCats = [{
+//         img:"./images/mr-whiskerson.png",
+//         name:"Mr. Whiskerson",
+//         phone="(212) 555-1234",
+//         email="mr.whiskaz@catnap.meow"
+//     },
+//     {
+//         img:"./images/fluffykins.png",
+//         name:"fluffylicios",
+//         phone="(212) 555-1234",
+//         email="mr.whiskaz@catnap.meow"
+//     },
+//     {
+//         img:"./images/felix.png",
+//         name:"Felix el gato",
+//         phone="(212) 555-1234",
+//         email="mr.whiskaz@catnap.meow"
+//     },
+//     {
+//         img:"./images/pumpkin.png",
+//         name:"senora calabaza",
+//         phone="(212) 555-1234",
+//         email="mr.whiskaz@catnap.meow"
+//     }]
+
+//     return (
+//         <div className="contacts">
+//          {
+//              dataCats.map(el => {
+//                  return (
+//                      <Contact img={el.img}
+//                                   name={el.name}
+//                                   phone={el.phone}
+//                                   email={el.email}
+//                  />
+//                  )
+//              })
+//          }
+//         </div>
+//     )
+// }
